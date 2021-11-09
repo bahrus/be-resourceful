69f025f90b774ddc7bd608d4840fccd9b02ce2c3
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-11-09 06:40:41 -0500

    Initial commit

--- be-resourceful.ts
+++ be-resourceful.ts
@@ -1,7 +1,7 @@
 import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
 import {BeResourcefulProps, BeResourcefulVirtualProps, BeResourcefulActions, Resource} from './types';
 import {register} from 'be-hive/register.js';
-
+declare const URLPattern: any;
 export class BeResourcefulController implements BeResourcefulActions {
     intro(proxy: Element & BeResourcefulVirtualProps, target: Element, bdp: BeDecoratedProps) {
         //  Create Resources Array Virtual Prop based on ifWantsToBe Attribute
@@ -34,7 +34,11 @@ export class BeResourcefulController implements BeResourcefulActions {
         Object.assign(proxy, virtualProps);
     }
     onResources({proxy, resources}: this){
-
+        for(const resource of resources!){
+            const p = new URLPattern(resource.URLPatternInit);
+            const result = p.match(window.location);
+            
+        }
     }
     createResource(path: string){
         //  Create Resource
